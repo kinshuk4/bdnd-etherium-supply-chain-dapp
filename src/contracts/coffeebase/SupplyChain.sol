852277132e667f2420cd3e682518609273ebf893
Kinshuk <kinshuk.ram@gmail.com>
2020-04-19 13:19:29 +0200

    Update all the modifiers

--- src/contracts/coffeebase/SupplyChain.sol
+++ src/contracts/coffeebase/SupplyChain.sol
@@ -1,6 +1,11 @@
 pragma solidity >=0.4.24;
 // Define a contract 'Supplychain'
-contract SupplyChain {
+import "../coffeeaccesscontrol/ConsumerRole.sol";
+import "../coffeeaccesscontrol/DistributorRole.sol";
+import "../coffeeaccesscontrol/FarmerRole.sol";
+import "../coffeeaccesscontrol/RetailerRole.sol";
+
+contract SupplyChain is ConsumerRole, DistributorRole, FarmerRole, RetailerRole {
 
   // Define 'owner'
   address payable owner;
@@ -96,43 +101,43 @@ contract SupplyChain {
 
   // Define a modifier that checks if an item.state of a upc is Processed
   modifier processed(uint _upc) {
-
+    require(items[_upc].itemState == State.Processed);
     _;
   }
   
   // Define a modifier that checks if an item.state of a upc is Packed
   modifier packed(uint _upc) {
-
+    require(items[_upc].itemState == State.Packed);
     _;
   }
 
   // Define a modifier that checks if an item.state of a upc is ForSale
   modifier forSale(uint _upc) {
-
+    require(items[_upc].itemState == State.ForSale);
     _;
   }
 
   // Define a modifier that checks if an item.state of a upc is Sold
   modifier sold(uint _upc) {
-
+    require(items[_upc].itemState == State.Sold);
     _;
   }
   
   // Define a modifier that checks if an item.state of a upc is Shipped
   modifier shipped(uint _upc) {
-
+    require(items[_upc].itemState == State.Shipped);
     _;
   }
 
   // Define a modifier that checks if an item.state of a upc is Received
   modifier received(uint _upc) {
-
+    require(items[_upc].itemState == State.Received);
     _;
   }
 
   // Define a modifier that checks if an item.state of a upc is Purchased
   modifier purchased(uint _upc) {
-    
+    require(items[_upc].itemState == State.Purchased);
     _;
   }
 
@@ -157,11 +162,11 @@ contract SupplyChain {
     string  memory _originFarmLatitude, string  memory _originFarmLongitude, string  memory _productNotes) public
   {
     // Add the new item as part of Harvest
-    
+
     // Increment sku
     sku = sku + 1;
     // Emit the appropriate event
-    
+
   }
 
   // Define a function 'processtItem' that allows a farmer to mark an item 'Processed'
