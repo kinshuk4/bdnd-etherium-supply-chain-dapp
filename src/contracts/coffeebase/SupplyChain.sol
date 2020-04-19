ba132df8c7266bc09d3cf9342bf45b9016fc5536
Kinshuk <kinshuk.ram@gmail.com>
2020-04-19 13:50:19 +0200

    Fix the fetchItem functions

--- src/contracts/coffeebase/SupplyChain.sol
+++ src/contracts/coffeebase/SupplyChain.sol
@@ -310,19 +310,14 @@ contract SupplyChain is ConsumerRole, DistributorRole, FarmerRole, RetailerRole 
     )
     {
         // Assign values to the 8 parameters
-
-
-        return
-        (
-        itemSKU,
-        itemUPC,
-        ownerID,
-        originFarmerID,
-        originFarmName,
-        originFarmInformation,
-        originFarmLatitude,
-        originFarmLongitude
-        );
+        itemSKU = items[_upc].sku;
+        itemUPC = items[_upc].upc;
+        ownerID = items[_upc].ownerID;
+        originFarmerID = items[_upc].originFarmerID;
+        originFarmName = items[_upc].originFarmName;
+        originFarmInformation = items[_upc].originFarmInformation;
+        originFarmLatitude = items[_upc].originFarmLatitude;
+        originFarmLongitude = items[_upc].originFarmLongitude;
     }
 
     // Define a function 'fetchItemBufferTwo' that fetches the data
@@ -340,19 +335,14 @@ contract SupplyChain is ConsumerRole, DistributorRole, FarmerRole, RetailerRole 
     )
     {
         // Assign values to the 9 parameters
-
-
-        return
-        (
-        itemSKU,
-        itemUPC,
-        productID,
-        productNotes,
-        productPrice,
-        itemState,
-        distributorID,
-        retailerID,
-        consumerID
-        );
+        itemSKU = items[_upc].sku;
+        itemUPC = items[_upc].upc;
+        productID = items[_upc].productID;
+        productNotes = items[_upc].productNotes;
+        productPrice = items[_upc].productPrice;
+        itemState = uint(items[_upc].itemState);
+        distributorID = items[_upc].distributorID;
+        retailerID = items[_upc].retailerID;
+        consumerID = items[_upc].consumerID;
     }
 }
