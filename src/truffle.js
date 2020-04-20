b0e269091fc1d54dd2e1a82adb2e8d5f0172b333
Kinshuk <kinshuk.ram@gmail.com>
2020-04-20 11:12:54 +0200

    Use the correct network place for rinkeby

--- src/truffle.js
+++ src/truffle.js
@@ -9,12 +9,12 @@ module.exports = {
       host: "127.0.0.1",
       port: 8545,
       network_id: "*" // Match any network id
-    }
-  },
-  rinkeby: {
-    provider: () => new HDWalletProvider(metamaskMnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
-    network_id: 4,       // rinkeby's id
-    gas: 4500000,        // rinkeby has a lower block limit than mainnet
-    gasPrice: 10000000000
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(metamaskMnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`),
+      network_id: 4,       // rinkeby's id
+      gas: 4500000,        // rinkeby has a lower block limit than mainnet
+      gasPrice: 10000000000
+    },
   },
 };
